2ea9f00d1292c3a6dca47b6ca66b8d1ef537f15d
Precious chiedu <udegbue69@gmail.com>
2020-09-18 19:54:28 +0100

    added test files

--- test/score.test.js
+++ test/score.test.js
@@ -1,16 +1,3 @@
 import {pushData} from '../src/score';
 
-const app = require('../src/score');
 
-describe('Post Endpoints', () => {
-  it('should create a new post', async () => {
-    const res = await request(app)
-      .post('/api/posts')
-      .send({
-        user: 'precious',
-        score: 42,
-      });
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty('post');
-  });
-});
