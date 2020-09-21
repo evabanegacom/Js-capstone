b27b6e99d6a08993c73e4cd5da31b6b174fa2047
Precious chiedu <udegbue69@gmail.com>
2020-09-21 11:52:08 +0100

    added tests

--- test/score.test.js
+++ test/score.test.js
@@ -1,36 +1,37 @@
 import { pushData, getData } from '../src/score';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve( result )
-  })
-);
+let result;
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve(result),
+}));
 
 beforeEach(() => {
   fetch.mockClear();
 });
 
-it("it gets users once", async () => {
+it('it gets users once', async () => {
+  /* eslint-disable */
+
   const rate = await getData();
 
-  //expect(rate).toEqual(1.42);
+  // expect(rate).toEqual(1.42);
   expect(fetch).toHaveBeenCalledTimes(1);
 });
 
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve( result[{ user: 'lui', score: 40}] )
-  })
-);
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve(result[{ user: 'lui', score: 40 }]),
+}));
 
 beforeEach(() => {
   fetch.mockClear();
 });
 
-it("to send users once", async () => {
+it('to send users once', async () => {
   const rate = await pushData('lui', 40);
+  /* eslint-enable */
 
-  //expect(rate).toEqual(1.42);
+  // expect(rate).toEqual(1.42);
   expect(fetch).toHaveBeenCalledTimes(1);
 });
